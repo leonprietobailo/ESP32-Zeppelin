2e9f3a17a0d6d50a136f34aa744ac55e7f133ef7
León E. Prieto Bailo <leon.enrique.prieto@estudiantat.upc.edu>
2023-12-26 12:09:21 +0100

    Added sensor fusion.

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -3,6 +3,10 @@
 #include <MPU6500_WE.h>
 #include <esp_task_wdt.h>
 #include <QMC5883LCompass.h>
+// #include <Fusion.h>
+
+#define SAMPLE_RATE = 100.0; // Hz
+uint64_t startGyro;
 
 // HMC5883 Initialization.
 QMC5883LCompass mag;
@@ -11,11 +15,11 @@ QMC5883LCompass mag;
 #define MPU6500_ADDR 0x68
 MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR);
 uint64_t executionTime;
-xyzFloat g, gyro;
+xyzFloat acc, vel, pos, g, gyro;
 
 // WiFi connection.
-const char *ssid = "XTA.CAT-7E5AA4";
-const char *password = "ZqqKpgwn";
+const char *ssid = "POCO X3 NFC";
+const char *password = "omegaisugly";
 WiFiServer server(80);
 
 // Function declarations
@@ -35,6 +39,28 @@ enum FlightMode
 
 FlightMode FLIGHT_MODE = FM_DISABLED;
 
+// const FusionMatrix gyroscopeMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+// const FusionVector gyroscopeSensitivity = {1.0f, 1.0f, 1.0f};
+// const FusionVector gyroscopeOffset = {0.0f, 0.0f, 0.0f};
+// const FusionMatrix accelerometerMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+// const FusionVector accelerometerSensitivity = {1.0f, 1.0f, 1.0f};
+// const FusionVector accelerometerOffset = {0.0f, 0.0f, 0.0f};
+// const FusionMatrix softIronMatrix = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+// const FusionVector hardIronOffset = {0.0f, 0.0f, 0.0f};
+
+// // Initialise algorithms
+// FusionOffset offset;
+// FusionAhrs ahrs;
+
+// const FusionAhrsSettings settings = {
+//     .convention = FusionConventionNwu,
+//     .gain = 0.5f,
+//     .gyroscopeRange = 2000.0f, /* replace this with actual gyroscope range in degrees/s */
+//     .accelerationRejection = 10.0f,
+//     .magneticRejection = 10.0f,
+//     .recoveryTriggerPeriod = 5 * 250, /* 5 seconds */
+// };
+
 void setup()
 {
   Wire.begin();
@@ -50,7 +76,19 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.print("Connecting to WiFi... Status: ");
+    Serial.println(WiFi.status()); // This will print the status code
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("Connected to WiFi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else
+  {
+    Serial.println("Failed to connect to WiFi. Check your credentials or hardware.");
   }
 
   // Start the server
@@ -62,7 +100,7 @@ void setup()
   xTaskCreatePinnedToCore(
       TaskServer,   /* Function to implement the task */
       "TaskServer", /* Name of the task */
-      100000,       /* Stack size in words */
+      10000,        /* Stack size in words */
       NULL,         /* Task input parameter */
       1,            /* Priority of the task */
       NULL,         /* Task handle. */
@@ -94,6 +132,12 @@ void setup()
   myMPU6500.enableAccDLPF(true);
   myMPU6500.setAccDLPF(MPU6500_DLPF_6);
   delay(200);
+
+  // FusionOffsetInitialise(&offset, 100);
+  // FusionAhrsInitialise(&ahrs);
+  // FusionAhrsSetSettings(&ahrs, &settings);
+
+  startGyro = micros();
 }
 
 void loop()
@@ -101,17 +145,19 @@ void loop()
   ReadUnits();
   Controllers();
   Actuators();
-  Diagnostics();
-
-  if (micros() - executionTime > 4000)
+  // Diagnostics();
+  Serial.println(micros() - executionTime);
+  if (micros() - executionTime > 1.0 / 100)
   {
-    Serial.println("WARNING: Long loop. PIDs may get unstable.");
+    Serial.println("WARNING: Long loop. PIDs may get unstable. " + micros() - executionTime);
   }
-  while (micros() - executionTime < 4000)
+  while (micros() - executionTime < 1 / 100)
   {
   }
   executionTime = micros();
 }
+int n = 0;
+xyzFloat g_avg;
 
 void ReadUnits()
 {
@@ -119,8 +165,72 @@ void ReadUnits()
   mag.read();
 
   // MPU6500 READOUTS
-  g = myMPU6500.getGValues();
+  // acc = myMPU6500.getCorrectedAccRawValues();
+  xyzFloat g_read = myMPU6500.getGValues();
+  g_avg += g_read;
+  g.x = g.x * 0.95 + g_read.x * 0.05;
+  g.y = g.y * 0.95 + g_read.y * 0.05;
+  g.z = g.z * 0.95 + g_read.z * 0.05;
   gyro = myMPU6500.getGyrValues();
+
+  // Velocity
+  // vel += g * 9.81 * 4000e-6;
+  // pos += vel * 4000e-6;
+
+  // const clock_t timestamp = (micros() - startGyro) / 1e6;
+  // FusionVector gyroscope = {gyro.x, gyro.y, gyro.z};
+  // FusionVector accelerometer = {g_read.x, g_read.y, g_read.z};
+
+  // gyroscope = FusionCalibrationInertial(gyroscope, gyroscopeMisalignment, gyroscopeSensitivity, gyroscopeOffset);
+  // accelerometer = FusionCalibrationInertial(accelerometer, accelerometerMisalignment, accelerometerSensitivity, accelerometerOffset);
+
+  // gyroscope = FusionOffsetUpdate(&offset, gyroscope);
+
+  // static clock_t previousTimestamp;
+  // const float deltaTime = (float)(timestamp - previousTimestamp) / (float)CLOCKS_PER_SEC;
+  // previousTimestamp = timestamp;
+
+  // FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, deltaTime);
+
+  // const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+  // const FusionVector earth = FusionAhrsGetEarthAcceleration(&ahrs);
+
+  // Serial.printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f, X %0.1f, Y %0.1f, Z %0.1f\n",
+  //               euler.angle.roll, euler.angle.pitch, euler.angle.yaw,
+  //               earth.axis.x, earth.axis.y, earth.axis.z);
+
+  // if (n == 10)
+  // {
+  //   vel += g_avg * 9.81 * n * 4000e-6 / n;
+  //   pos += vel * 4000e-6 + g_avg * 9.81 * n * 4000e-6 * 0.5 * n * 4000e-6;
+  //   n = 0;
+  // }
+  // else
+  // {
+  //   n++;
+  // }
+
+  // Serial.print("gx: ");
+  // Serial.print(g.x);
+  // Serial.print("\t");
+
+  // Serial.print("gy: ");
+  // Serial.print(g.y);
+  // Serial.print("\t");
+
+  // Serial.print("gz: ");
+  // Serial.print(g.z);
+
+  // Serial.print("x: ");
+  // Serial.print(pos.x);
+  // Serial.print("\t");
+
+  // Serial.print("y: ");
+  // Serial.print(pos.y);
+  // Serial.print("\t");
+
+  // Serial.print("z: ");
+  // Serial.println(pos.z);
 }
 void Controllers()
 {
